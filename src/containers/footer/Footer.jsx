65f20215fa7434fcb2536d769a68c492234bdf5c
Olufeyimi Samuel <donsmog@gmail.com>
2022-01-13 15:05:29 +0100

    footer

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="gpt3__footer section__padding">
+      Footer
+      <h3>Footer</h3>
+      <h3>Footer</h3>
+      <h3>Footer</h3>
+      <h3>Footer</h3>
+    </div>
+  );
+};
+
+export default Footer;
